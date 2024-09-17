762e0be984c0774d1b6ecf9b37e14d2fa361f0da
NewUser <newuser@NewUsers-MacBook-Pro.local>
2024-09-17 13:23:22 +1000

    initializing spring boot project

---
+++ src/main/java/net/javaguides/springboot/HelloWorldController.java
@@ -0,0 +1,11 @@
+package net.javaguides.springboot;
+
+import org.springframework/stereotype.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloWorldController {
+
+}
